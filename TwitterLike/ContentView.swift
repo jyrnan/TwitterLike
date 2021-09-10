efaae912023b516f7bc9f073e28601276e36025e
jyrnan <yoeking@gmail.com>
2021-09-10 10:30:25 +0800

    Initial Commit

---
+++ TwitterLike/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterLike
+//
+//  Created by jyrnan on 2021/9/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterLike/TwitterLikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterLikeApp.swift
+//  TwitterLike
+//
+//  Created by jyrnan on 2021/9/10.
+//
+
+import SwiftUI
+
+@main
+struct TwitterLikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
